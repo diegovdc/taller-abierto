9f0cf3141fbdc6436e3dfaef267e8c7d42b00fe9
Diego <diego.vid.eco@gmail.com>
2019-12-28 22:39:29 -0600

    Removes nanc in a can tests

--- test/nanc_in_a_can/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns nanc-in-a-can.core-test
-  (:require [clojure.test :refer :all]
-            [time-time.converge :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
