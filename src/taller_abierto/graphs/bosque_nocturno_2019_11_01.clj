bc334632609637d3e195fdd76edb03a3478a342f
Diego <diego.vid.eco@gmail.com>
2020-01-18 13:22:45 -0600

    Fixes node name

--- src/taller_abierto/graphs/bosque_nocturno_2019_11_01.clj
+++ src/taller_abierto/graphs/bosque_nocturno_2019_11_01.clj
@@ -27,35 +27,34 @@
       (o/distort sig)
       (o/out 0 sig))))
 
-(gas->crystal i/a1 :bpf-start 200 :bpf-end 300 :start-at 88000 :amp 10)
+#_(gas->crystal i/a1 :bpf-start 200 :bpf-end 300 :start-at 88000 :amp 10)
 
 (defn synth*
   [& {:keys [vals metronome index start-pos sample pan amp]}]
-  (println index )
   (let [r1 (rrange 5000 7000)
         r2 (rrange 2000 10000)]
     (gas->crystal sample
                   :amp 100
                   :depth 0.7
                   :bpf-start (rand-nth [r1 r2])
-                  :bpf-end (rand-nth [r1 r2])
+                  :bpf-end  (rand-nth [r1 r2])
                   :start-pos start-pos
                   :dur (:dur vals)
                   :rate (+- 1 (rand)))))
 
 (def insectos
   "usar :voicef"
-  {:instruments [i/a1]
+  {:instruments [i/a1 i/a7]
    :synth #'synth*})
 
 (def graph {#'insectos #{#'insectos}})
 
 
-(def xos (->xos "x"))
+(def xos (->xos "xooxo"))
 (defonce state (atom {:history [] :xos #'xos}))
-(swap! state assoc :history [#'vision-total])
-(comment (swap! state assoc :voicef #{7 8 9 10 12 20 21}
-                ))
+(swap! state assoc :history [#'insectos])
+(comment (swap! state assoc :voicef #{7 8 9 10 12 20 21}))
+(comment (swap! state assoc :voicef true))
 
 (defn mirror [xs] (concat xs (reverse xs)))
 
@@ -64,7 +63,7 @@
                                      flatten)
                           :tempos (->> [7 5] (repeat 10) flatten)
                           :cps [10]
-                          :period (* 5 60)
+                          :period (* 2 60)
                           :bpm 60})
              2  (converge {:durs (->> [7 5 5 7 5 5]
                                       (repeat 2)
@@ -93,7 +92,7 @@
   (g/play-next! state graph)
   (o/stop)
   (def xos (->xos "x"))
-  (def viento (sample-canon state (canons 3)))
+  (def viento (sample-canon state (canons 1)))
   (meta (canons 2)))
 
 (comment (ctl-list state #(o/ctl % :pan 1)))
