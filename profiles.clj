a95a3d8d41ee34d6d4e118a37e3db5418d57e060
Diego <diego.vid.eco@gmail.com>
2020-04-12 21:08:07 -0500

    Fixes typo and adds combinatorics lib

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
 {:repl {:plugins [[refactor-nrepl "2.5.0-SNAPSHOT"]]}
 :dev {:source-paths ["dev"]
       :dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]
-                      [org.clojure/test.check "0.10.0"]]}}
+                     [org.clojure/math.combinatorics "0.1.6"]
+                     [org.clojure/test.check "0.10.0"]]}}
